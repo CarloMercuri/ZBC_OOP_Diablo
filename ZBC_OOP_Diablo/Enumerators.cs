6c0fd86190ce86cfa9931f27349fa3311194931b
Carlo Mercuri <cmercuri85@gmail.com>
2021-09-13 21:40:35 +0200

    Add project files.

---
+++ ZBC_OOP_Diablo/Enumerators.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZBC_OOP_Diablo
+{
+    public enum WeaponType
+    {
+        Axe,
+        Dagger,
+        Mace,
+        Spear,
+        Sword,
+        CeremonialKnife,
+        FistWeapon,
+        Flail,
+        MightyWeapon,
+        Scythe
+    }
+
+    public enum WeaponQuality
+    {
+        Blue = 0,
+        Yellow = 1,
+        Legendary = 2
+    }
+    
+    public enum WeaponCategory
+    {
+        OneHanded,
+        TwoHanded
+    }
+}
---
+++ ZBC_OOP_Diablo/Models/IWeaponAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZBC_OOP_Diablo
+{
+    public interface IItemAttribute
+    {
+        /// <summary>
+        /// The minimum values for 1 handed weapons as array. 0 = blue, 1 = yellow, 2 = legendary
+        /// </summary>
+        float[] Min1HValues { get; set; }
+        
+        float[] Min2HValues { get; set; }
+       
+        /// <summary>
+        /// The value that has been locked in place
+        /// </summary>
+        float Value { get; set; }
+    }
+}
